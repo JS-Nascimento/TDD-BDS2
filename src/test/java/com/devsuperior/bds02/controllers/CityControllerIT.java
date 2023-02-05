9ee887c847b5592f0488e732052d86713d65d19a
JS-Nascimento <jorge@jstec.dev.br>
2023-02-05 10:03:37 -0300

    units tests adjust

--- src/test/java/com/devsuperior/bds02/controllers/CityControllerIT.java
+++ src/test/java/com/devsuperior/bds02/controllers/CityControllerIT.java
@@ -1,11 +1,7 @@
 package com.devsuperior.bds02.controllers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.devsuperior.bds02.dto.CityDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,11 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.bds02.dto.CityDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,7 +26,8 @@ public class CityControllerIT {
 	
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+	private Object Propagation;
+
 	@Test
 	public void findAllShouldReturnAllResourcesSortedByName() throws Exception {
 		
@@ -84,8 +82,8 @@ public class CityControllerIT {
 	}
 
 	@Test
-	@Transactional(propagation = Propagation.NEVER) 
-	public void deleteShouldReturnBadRequestWhenDependentId() throws Exception {		
+	@Transactional
+	public void deleteShouldReturnBadRequestWhenDependentId() throws Exception {
 
 		Long dependentId = 1L;
 		
